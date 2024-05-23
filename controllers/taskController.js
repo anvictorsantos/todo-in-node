6ecdcd225c5239d19799ba940ed036234778d103
Antonio Pontes <anvictorsantos@gmail.com>
2024-05-23 20:33:32 +0100

    feat: task controller

---
+++ controllers/taskController.js
@@ -0,0 +1,49 @@
+const Task = require("../models/task");
+
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.getAllTasks();
+    res.json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getTaskById = async (req, res) => {
+  try {
+    const task = await Task.getTaskById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.createTask(req.body);
+    res.status(201).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.updateTask(req.params.id, req.body);
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    const deletedTask = await Task.deleteTask(req.params.id);
+    res.json(deletedTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
